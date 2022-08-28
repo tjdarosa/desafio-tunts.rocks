15775453b41885e2aeae005d1943a1b82ba4abdc
Tiago <tjdarosa1@gmail.com>
2022-08-28 18:50:31 -0300

    code refactoring

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ messages can be displayed at the console.
 */
 async function getData(){
     try{
-        const data = axios.get("https://restcountries.com/v3.1/all");
-        if (await data){
-            console.log("data collected","\nresponse_status_code:",(await data).status,"\n");
-            return (await data).data
+        const data = await axios.get("https://restcountries.com/v3.1/all");
+        if (data){
+            console.log("data collected","\nresponse_status_code:", data.status,"\n");
+            return data.data
         } else{
             console.log('Something went wrong!');
         }
@@ -26,27 +26,43 @@ about only one country (only the requested data for the
 creation of the sheet are put in the array: Name, Capital,
 Area and Currencies)
 */
-async function buildCountriesArray(receivedData){
+function buildCountriesArray(receivedData){
     let countriesArray = []
-    for (countryData of await(receivedData)){
+    for (countryData of receivedData){
         let country = {}
 
-        try{country.Name = countryData.name.common}
-        catch{country.name = "-"}
+        // Get the name of the country
+        if(countryData?.name?.common){
+            country.Name = countryData?.name?.common
+        }else{
+            country.Name = "-"
+        }
 
-        try{country.Capital = countryData.capital.toString()}
-        catch{country.capital = "-"}
+        // Get the capital of the country
+        if(countryData?.capital){
+            country.Capital = countryData.capital.toString()
+        }else{
+            country.Capital = "-"
+        }
 
-        try{country.Area = countryData.area.toLocaleString('de-DE',{
-            style: "decimal",
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
-        })}
-        catch{country.area = "-"}
+        // Get the area of the country
+        if(countryData?.area){
+            country.Area = countryData.area.toLocaleString('pt-BR',{
+                style: "decimal",
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
+            })
+        }else{
+            country.Area = "-"
+        }
 
-        try{country.Currencies = Object.keys(countryData.currencies).toString()}
-        catch{country.currencies = "-"}
-    
+        // Get the currencies of the country
+        if(countryData?.currencies){
+            country.Currencies = Object.keys(countryData.currencies).toString()
+        }else{
+            country.Currencies = "-"
+        }
+
         countriesArray.push(country)
     }
     return countriesArray
@@ -60,11 +76,11 @@ and iterates through each object of the array (and throught each
 value of each object) and writes them in the file.
 */
 async function createSheet(countriesArray){
-    var wb = new xl.Workbook();
-    var ws = wb.addWorksheet('Sheet 1')
+    const wb = new xl.Workbook();
+    const ws = wb.addWorksheet('Sheet 1');
 
     // style for the title
-    var titleStyle = wb.createStyle({
+    const titleStyle = wb.createStyle({
         font: {
           color: '#4F4F4F',
           size: 16,
@@ -77,7 +93,7 @@ async function createSheet(countriesArray){
     });
 
     //style for the headers
-    var headerStyle = wb.createStyle({
+    const headerStyle = wb.createStyle({
         font: {
           color: '#808080',
           size: 12,
@@ -86,16 +102,16 @@ async function createSheet(countriesArray){
     });
 
     // write the title in the sheet
-    var title = "CountriesList";
+    const title = "Countries List";
     ws.cell(1, 1, 1, 4, true).string(title).style(titleStyle);
 
     // write the headers in the sheet
-    var headers = Object.keys(countriesArray[0]);
-    var count = 1;
+    const headers = Object.keys(countriesArray[0]);
+    let count = 1;
     for (header of headers){
         ws.cell(2,count).string(header).style(headerStyle);
         count ++;
-    }
+    };
 
     // fill the sheet with data 
     rowCount = 3
@@ -104,21 +120,25 @@ async function createSheet(countriesArray){
     for(country of countriesArray){
         dataArray = Object.values(country);
         for(data of dataArray){
-            ws.cell(rowCount,colCount).string(data)
+            ws.cell(rowCount,colCount).string(data);
             colCount++;
         }
         colCount = 1;
         rowCount ++;
     }
 
-    wb.write("CountriesList.xlsx")
+    wb.write(".xlsx/CountriesList.xlsx")
 }
 
+//Here is the main function, from where the application execution begins
 async function main(){
-    const data_colected = getData()
-    if(await (data_colected)){
-        const countriesArray = buildCountriesArray(data_colected)
-        createSheet(await(countriesArray))
+    // first, the data is collected via http request
+    const dataCollected = await getData()
+    if(dataCollected){
+        // if the data can be got from the API, the array of countries is built
+        const countriesArray = buildCountriesArray(dataCollected)
+        // then the sheet is created based on the data contained in the array
+        createSheet(countriesArray)
     }
 }
 
